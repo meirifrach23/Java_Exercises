db2fbd1083b0e7d8e5ebe12dc33bdd1829be343e
= <=>
2017-06-26 15:22:44 +0300

    Tattoo removal exercise

---
+++ src/Tattoo_Removal/Main.java
@@ -0,0 +1,23 @@
+package Tattoo_Removal;
+
+import java.io.IOException;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        //printFile();
+        //File file = new File("C:/incoming/text.txt");
+
+        //readContentIntoByteArray(file);
+
+        //int cc = TextFilesClass.readFromPosition("C:/incoming/text.txt", 1);
+        //System.out.println(cc);
+        //TextFilesClass.writeToPosition("C:/incoming/text2.txt", cc, 0);
+        TextFilesClass.replaceCharacters("C:/incoming/text.txt");
+    }
+
+
+
+
+}
